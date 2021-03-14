ac0145d4e6417e69ff63931873a1ef17527420dc
Maksim <maks.panasenko01@bk.ru>
2021-03-14 04:00:41 +0300

    Init

---
+++ ShannonFano/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShannonFano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
